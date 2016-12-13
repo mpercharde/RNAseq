acf3763b902d52cd6e8272209c1d6c494851d7f0
mush <mpercharde@otp545133ots.ucsf.edu>
2016-12-13 11:23:29 -0800

    real program commands

--- runRNAseq2.sh
+++ runRNAseq2.sh
@@ -37,7 +37,7 @@ while getopts ':gchi:' flag; do
   	       echo "    -h        Help mode, prints Usage"
   	       echo "    -g        Input FASTQ is .gz compressed"
            echo "    -c        Libraries are CLONTECH"
-  	       echo "    -i        input file directory/. use "./" for current dir (not rec)"
+  	       echo "    -i        input file directory/. use "./" for current dir (not recommended)"
   	       echo ""
            flagcheck=1
            exit 1 ;;
@@ -76,37 +76,29 @@ for file in "$dir"* ; do
 
     if [ "$clontech" == "true" ]; then
       echo ""
-      echo "analyzing clontech libraries, adaptor trimming will include clipping 3bp"
-      trim=$(echo "$name is clontech")
+      echo "$name is a clontech library, adaptor trimming will include clipping 3bp"
       echo ""
+      trim="trim_galore --clip_R 3"
     else
       echo ""
-      trim=$(echo "$name is nebnext")
+      echo "$name is an nebnext library"
       echo ""
+      trim="trim_galore"
     fi
-
-    # if [ "$clontech" == "true" ]; then
-      # trim = "trim_galore --clip_R 3" --fastqc --fastqc_args " --outdir trimmed/fastqc/" --illumina $file -o trimmed/
-      # trim = "trim_galore" trim_galore --clip_R 3 --fastqc --fastqc_args " --outdir trimmed/fastqc/" --illumina $file -o trimmed/
-    # else
-    #   trim = "trim_galore --fastqc --fastqc_args " --outdir trimmed/fastqc/" --illumina $file -o trimmed/"
-    # fi
 
     echo "1. trimming $name"
     echo ""
-    echo $trim
-    echo ""
-    # trim_galore --fastqc --fastqc_args " --outdir trimmed/fastqc/" --illumina $file -o trimmed/ #check what to happen if not gz.
+    $trim --fastqc --fastqc_args " --outdir trimmed/fastqc/" --illumina $file -o trimmed/
 
     echo "2. aligning $name to mm10 plus ERCCs"
     echo ""
     mkdir ${name}_aligned/
-  # tophat -o ${name}_aligned/ -p 4 -g 20 -G /data/refs/mm10/BTmm10_ercc/genes_ercc.gtf --no-coverage-search --library-type fr-firststrand \
-  # --no-novel-indels /data/refs/mm10/BTmm10_ercc/mm10_ercc trimmed/${name}_trimmed.fq
+    tophat -o ${name}_aligned/ -p 4 -g 20 -G /data/refs/mm10/BTmm10_ercc/genes_ercc.gtf --no-coverage-search --library-type fr-firststrand \
+    --no-novel-indels /data/refs/mm10/BTmm10_ercc/mm10_ercc trimmed/${name}_trimmed.fq  ###THIS WON"T WORK WITH .GZ currently
 
     echo "3. sorting $name bam file ready for DL"
     echo ""
-    # samtools sort -o sorted_bam/${name}.sorted.bam ${name}_aligned/accepted_hits.bam
+    samtools sort -o sorted_bam/${name}.sorted.bam ${name}_aligned/accepted_hits.bam
     echo "$name DONE!"
     echo ""
 done
