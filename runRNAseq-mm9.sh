c0eea4f4b0f48e5df23c9835129b96de391e975a
mpercharde <michelle.percharde@ucsf.edu>
2017-12-07 13:25:17 -0800

    update: mm9 SE no ERCC script

--- runRNAseq-mm9.sh
+++ runRNAseq-mm9.sh
@@ -4,19 +4,25 @@
 #       RNAseq analysis pipeline                  #
 #       Michelle Percharde, PhD 2016              #
 #                                                 #
-#                v.mm9                            #
+#                v.mm9      no ERCC               #
 ###################################################
 
 #~~~~~~~~~~EDIT BELOW THIS LINE ~~~~~~~~~~~~~~~~~~#
 
+# THIS USES TRANSCRIPTOME ALIGNMENT -
+# CHECK IF YOU HAVE THE KNOWN_GENES FOLDER
+# IF NOT: FOR THE FIRST TIME RUN TRANSCRIPTOME INDEXING ALONE SEE BELOW, BEFORE SCRIPT. THEN RUN SCRIPT
+
 # Pipeline to take input dir with files "sample.fq" or "sample.fq.gz", outputs sorted bams
 # Bam files can then be DLed and fed into FeatureCounts in R
 
-#usage: ./runRNAseq2.sh [options] [-i path/to/folder/]
+#usage: ./runRNAseq-mm9.sh [options] [-i path/to/folder/]
 
 #FOLDERS NEEDED IN ROOT:
   #raw/ (where raw files are)
 
+  # echo "indexing the transcriptome data one time - prefix mm9" ###DO THIS FIRST TIME - run in ref/mm9 folder ###
+    # tophat -G /data/refs/mm9/mm9.gtf --transcriptome-index=transcriptome_data/known_genes
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 flagcheck=0
@@ -76,7 +82,7 @@ for file in "$dir"* ; do
       echo ""
       echo "$name is a clontech library, adaptor trimming will include clipping 3bp"
       echo ""
-      trim="trim_galore --clip_R 3"
+      trim="trim_galore --clip_R1 3"
     else
       echo ""
       echo "$name is an nebnext library"
@@ -84,6 +90,12 @@ for file in "$dir"* ; do
       trim="trim_galore"
     fi
 
+    echo ""
+    echo "#######################################"
+    echo "## RNA-seq pipeline - mm9 SE         ##"
+    echo "##   by Michelle Percharde, PhD      ##"
+    echo "##   mpercharde@gmail.com            ##"
+    echo "#######################################"
     echo "1. trimming $name"
     echo ""
     $trim --fastqc --fastqc_args " --outdir trimmed/fastqc/" --illumina $file -o trimmed/
@@ -92,8 +104,10 @@ for file in "$dir"* ; do
     echo "2. aligning $name to mm9 standard"
     echo ""
     mkdir -p ${name}_aligned/
-    tophat -o ${name}_aligned/ -p 4 -g 20 -G /data/refs/mm9/mp_genes.gtf --no-coverage-search --library-type fr-firststrand \
-    --no-novel-indels /data/refs/mm9/mm9 trimmed/$trimfile ###prev ${name}_trimmed.fq
+    tophat -o ${name}_aligned/ -p 4 -g 20 --no-coverage-search --library-type fr-firststrand \
+    --no-novel-indels --transcriptome-index=/data/refs/mm9/transcriptome_data/known_genes /data/refs/mm9/mm9 trimmed/$trimfile
+    # tophat -o ${name}_aligned/ -p 4 -g 20 -G /data/refs/mm9/mp_genes.gtf --no-coverage-search --library-type fr-firststrand \
+    # --no-novel-indels /data/refs/mm9/mm9 trimmed/$trimfile ###prev ${name}_trimmed.fq
 
     echo ""
     echo "3. sorting $name bam file ready for DL"
